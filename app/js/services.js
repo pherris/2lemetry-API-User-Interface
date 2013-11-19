6e21a93c4259498393b076b8bd2d441b36cfc3d2
Pherris <aralston@peoplenetonline.com>
2013-11-19 12:26:16 -0600

    bootstrap makes things pretty

--- app/js/services.js
+++ app/js/services.js
@@ -6,9 +6,9 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 var serviceModule = angular.module('2lemetryApiV2.services', ['ngResource']).
-    value('version', '0.0.1').
+    value('version', '0.1.0').
     factory('AuthService',function ($http) {
-        //$http is recommended for cases where you have to pass in variables (really) - or maybe I don't know what I'm doing...
+        // $http is recommended for cases where you have to pass in variables (really) - or maybe I don't know what I'm doing...
         return {
             auth: function (username, password, successCb, errorCb) {
                 $http.get('https://api.m2m.io/2/auth', {
@@ -19,12 +19,12 @@ var serviceModule = angular.module('2lemetryApiV2.services', ['ngResource']).
             }
         };
     }).
-    //creates a persistent store to use for immutable (really immutable or probably
+    // creates a persistent store to use for immutable (really immutable or probably
     // immutable for the duration of the user's session) data to be retained across screens
     factory('PersistedData', function ($rootScope) {
         return {
             setDataSet: function (name, object) {
-                //if the object doesnt say when it was created...
+                // if the object doesnt say when it was created...
                 if (!object.created) {
                     object.created = (new Date()).getTime();
                 }
@@ -33,7 +33,7 @@ var serviceModule = angular.module('2lemetryApiV2.services', ['ngResource']).
             },
             getDataSet: function (name) {
                 var dataSet = JSON.parse(localStorage.getItem(name));
-                //for data in local storage - don't let it be older than 3 hrs
+                // for data in local storage - don't let it be older than 3 hrs
                 if (dataSet && dataSet.created < ((new Date()).getTime()) - 1000 * 60 * 60 * 3) {
                     localStorage.removeItem(name);
                 }
@@ -51,7 +51,7 @@ serviceModule.factory('m2m', ['PersistedData', '$resource', '$http', function (P
             var domain = PersistedData.getDataSet('Domain');
             return (domain && domain.rowkey) ? domain.rowkey : "";
         } }),
-        //TODO: pull all these Account interactions together.
+        // TODO: pull all these Account interactions together.
         Account: $resource('https://api.m2m.io/2/account/:email', {email: '@email'}),
         AccountPwd: $resource('https://api.m2m.io/2/account/:rowkey', {rowkey: '@rowkey'}, {
         	change: {
@@ -71,7 +71,7 @@ serviceModule.factory('m2m', ['PersistedData', '$resource', '$http', function (P
                     var domain = PersistedData.getDataSet('Domain');
                     return domain.rowkey;
                 }, clearAcl: true, email: '@email', password: '@password' },
-                isArray: true, //limits permissions
+                isArray: true, // limits permissions
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
                 }
